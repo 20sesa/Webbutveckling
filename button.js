52fc4a152fecfe916e476697efda4a8f5558ccea
20sesa <20sesa@stockholmscience.se>
2022-10-03 14:07:19 +0200

    Lagt till Footer och knappar
    Jag har lagt till en Footer med min kontaktinformation och jag har lagt till Hero knappar där vart en funkar. Jag hade lite problem med att knapparna inte låg vart det skulle men jag fixade det genom att ge dem flex propertyn och genom att fimpla lite med flex inställningarna såsom att ge dem en wrap. Jag har också lagt till några kvalite ändringar för att göra sidan mer användarvänlig på mobil.

---
+++ button.js
@@ -0,0 +1,9 @@
+let mybutton = document.getElementById("TopButton")
+
+function TopFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+} 
+
+TopFunction() = document.getElementById("Top").scrollIntoView();
+MidFunction() = document.getElementById("AboutMe").scrollIntoView
