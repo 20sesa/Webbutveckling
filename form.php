aa69949919366130e4ee44e0695db2edd858dfd2
20sesa <20sesa@stockholmscience.se>
2022-10-06 10:07:48 +0200

    Fel med ankoppling till databas
    Jag fixade gradienten på hero imagen. Sedan försökte jag fixa till så att man kunde skicka data till databasen. Jag har lyckats efter några fel att komma till delen då koden kan försöka logga in på databasen. Dock gör den något fel eller så är det något som håller den borta från att koppla in och det är vad jag skall försöka fixa.

--- form.php
+++ form.php
@@ -1,5 +1,9 @@
 <?php
-    include_once "../public_html/DB.php";
+    include_once "../../private_html/Details.php";
+
+    $pdoparam = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+    $db = new PDO('mysql:host='.$db_host.';dbname='.$db_db, $db_user, $db_pass, $pdoparam);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
     $sql_query = "INSERT INTO `contact` (`id`, `name`, `gmail`, `message`)
     VALUES (NULL,:firstname, :lastname, :gmail, :contactmessage);"; 
